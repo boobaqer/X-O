3396b99f4ab223b8ddbd0d23871f7d617cce911f
Ali Mohammad Baqer <boobaqer@icloud.com>
2020-05-07 23:57:57 +0300

    Initial Commit

---
+++ X-O/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  X-O
+//
+//  Created by Ali-mac Baqer on 5/7/20.
+//  Copyright © 2020 AliBaqer. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
